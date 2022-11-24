5a883e0d244d76e341f451a5952afde5e13039a7
Whitney Muchira <whitneymuchira@gmail.com>
2022-11-24 10:22:54 +0300

    object constructor fun

--- class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,2 +1,19 @@
 //Create a constructor with 4 properties and 3 methods
+function Pizza(crust,flavour,brand,size){
+    this.crust=crust
+    this.flavour=flavour
+    this.brand=brand
+    this.size=size
 
+    this.toppings=function(){
+        console.log(`Pineapple ${flavour}`)
+    }
+    this.DeliveryTime=function(){
+        console.log("DELIVERY TIME VARIES!!")
+    }
+    this.sauce=function(){
+        console.log("Make it thick!!")
+    }
+}
+let BestPizza=new Pizza("Thick", "BBQ Chicken", "Dominos", "Chairman")
+Pizza.prototype.cheese=true
