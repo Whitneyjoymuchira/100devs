1bb0147947d013f07712f7334adc19fa976b8a70
Whitney Muchira <whitneymuchira@gmail.com>
2022-11-24 23:44:38 +0300

    beginner api files

--- class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,19 +1,41 @@
 //Create a constructor with 4 properties and 3 methods
-function Pizza(crust,flavour,brand,size){
-    this.crust=crust
-    this.flavour=flavour
-    this.brand=brand
-    this.size=size
+// function Pizza(crust,flavour,brand,size){
+//     this.crust=crust
+//     this.flavour=flavour
+//     this.brand=brand
+//     this.size=size
 
-    this.toppings=function(){
+    // this.toppings=function(){
+    //     console.log(`Pineapple ${flavour}`)
+    // }
+    // this.DeliveryTime=function(){
+    //     console.log("DELIVERY TIME VARIES!!")
+    // }
+    // this.sauce=function(){
+    //     console.log("Make it thick!!")
+    // }
+// }
+
+class Pizza{
+    constructor(crust,flavour,brand,size){
+        this.crust=crust
+        this.flavour=flavour
+        this.brand=brand
+        this.size=size
+    }
+   //inefficient
+    toppings(){
         console.log(`Pineapple ${flavour}`)
     }
-    this.DeliveryTime=function(){
+    DeliveryTime(){
         console.log("DELIVERY TIME VARIES!!")
     }
-    this.sauce=function(){
-        console.log("Make it thick!!")
-    }
+
+
 }
 let BestPizza=new Pizza("Thick", "BBQ Chicken", "Dominos", "Chairman")
+//efficient prototyping
 Pizza.prototype.cheese=true
+Pizza.prototype.sauce=function(){
+    console.log("Make it thick!!")
+}
--- class26-materials/class26-materials/objects-shots/js/main.js
+++ class26-materials/class26-materials/objects-shots/js/main.js
@@ -1,1 +1,24 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+document.querySelector('button').addEventListener('click', getDrink)
+let Name=document.querySelector('h2')
+let instructions=document.querySelector('h3')
+let img=document.querySelector('img')
+
+function getDrink()
+{
+  let drink=  document.querySelector('input').value
+
+
+ fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+.then(res=> res.json())
+.then(data=>{
+Name.innerText=data.drinks.strDrink
+instructions.innerText=data.drinks[2].strInstructions
+img.src=data.drinks[2].strDrinkThumb
+})
+.catch(err =>{
+console.log(`error ${err}`)
+})
+}
+
